1fd52464de00ea75866a6b10dfab0b1b2fae460c
Raymond Lee <raymanl33@icloud.com>
2021-10-23 18:45:39 -0700

    add replace static dat awith dynamic content from myForm.ejs to index.ejs

--- Movieflix_starter/app.js
+++ Movieflix_starter/app.js
@@ -10,14 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+const MESSAGES = { SUCCESS: "This movie exist in the database!", FAILURE: "This movie does not exist in the databse!"};
 
 app.get("/", (req, res) => res.render("pages/index"));
 
 app.get("/myForm", (req, res) => res.render("pages/myForm"));
 
-app.post("myForm", (req, res) => {
-  let formData = req.body;
-  console.log(formData)
+app.post("/myForm", (req, res) => {
+  let movieData = req.body;
+  const movieList = movieData['MovieID'].split(', ')
+  console.log(movieList)
+  res.render("pages/index", { movies: movieList});
 });
 
 app.get("/myListQueryString", (req, res) => {
